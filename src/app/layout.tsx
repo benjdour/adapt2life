90ed73fb4e8d9d367748bdf1f939acabc0fe5d7e
Benjamin Dour <benjamin@5e:35:dc:99:3a:46.home>
2025-10-09 17:21:57 -0400

    Initial commit of Adapt2Life website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
