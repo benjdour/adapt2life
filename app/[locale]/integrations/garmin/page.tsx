7767b32a6c3fd9ea0303454cc967103cc1db3b5d
Benjamin Dour <benjdour@gmail.com>
2025-12-09 17:54:29 -0500

    feat: localize Garmin integration page and actions

--- app/[locale]/integrations/garmin/page.tsx
+++ app/[locale]/integrations/garmin/page.tsx
@@ -1,2 +1,2 @@
-export { metadata } from "@/app/integrations/garmin/page";
+export { generateMetadata } from "@/app/integrations/garmin/page";
 export { default } from "@/app/integrations/garmin/page";
